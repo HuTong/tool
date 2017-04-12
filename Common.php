2df1928fdc7fa78694e5ff7e2f8bef2cc67a6f7f
hutong <hutong0521@163.com>
2017-04-12 13:59:20 +0800

    获取ip和判断是否用了代理

--- Common.php
+++ Common.php
@@ -60,4 +60,54 @@ class Common
 
         return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . ' ' . $unit[$i];
     }
+
+	/**
+	 * 判断是否使用代理 只能判断一般的普通代理和通过header模拟ip的情况
+	 *
+	 * 普通代理一般存在 HTTP_VIA/HTTP_X_FORWARDED_FOR/HTTP_X_PROXY_ID 等特性
+	 * HTTP_X_FORWARDED_FOR HTTP_CLIENT_IP  可以通过header模拟 不能提供参考
+	 *
+	 * @return boolean
+	 * @author hutong
+	 * @date   2017-04-12T13:27:52+080
+	 */
+	public static function isProxy()
+	{
+		if(isset($_SERVER['HTTP_VIA']))
+		{
+			return true;
+		}elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] != $_SERVER['REMOTE_ADDR']){
+			return true;
+		}elseif(isset($_SERVER['HTTP_CLIENT_IP']) && $_SERVER['HTTP_CLIENT_IP'] != $_SERVER['REMOTE_ADDR']){
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * 获取客户端访问IP
+	 * 因为客户端可以模拟 HTTP_X_FORWARDED_FOR和HTTP_CLIENT_IP 所以获取ip的时候不考虑
+	 *
+	 * @return string
+	 * @author hutong
+	 * @date   2017-04-12T13:46:04+080
+	 */
+	public function getIp($ignoreProxy = true)
+	{
+		if($ignoreProxy)
+		{
+			return getenv('REMOTE_ADDR');
+		}else{
+			if(getenv('HTTP_X_FORWARDED_FOR'))
+			{
+				$ip = getenv('HTTP_X_FORWARDED_FOR');
+			}elseif(getenv('HTTP_CLIENT_IP')){
+				$ip = getenv('HTTP_CLIENT_IP');
+			}else{
+				$ip = getenv('REMOTE_ADDR');
+			}
+			return $ip;
+		}
+	}
 }
